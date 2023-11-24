de3e3c8384380e579c597c4217e3ab4e6709d17f
Marcus Hert Da Coregio <marcusdacoregio@gmail.com>
2023-11-24 10:40:04 -0300

    Add Max Sessions on WebFlux Sample

---
+++ reactive/webflux/java/session-management/maximum-sessions/src/main/java/com/example/maximumsessions/MaximumSessionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.maximumsessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximumSessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximumSessionsApplication.class, args);
+	}
+
+}
